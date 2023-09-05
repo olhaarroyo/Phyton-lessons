42649a1090247d1a908e2f940c6908937fe5b9c1
o.savchuk.arroyo <arroyosavcuk@gmail.com>
2023-09-05 12:20:14 +0200

    initial commit

---
+++ main.py
@@ -0,0 +1,25 @@
+
+# Завдання 1
+# number1 = 8
+# number2 = 3
+# number3 = 4
+# sum_result = number1 + number2 + number3
+# print("result:", sum_result)
+
+#Завдання 3
+
+# number = 3553
+# number_str = str(number)
+# product_result = 1
+# for digit in number_str:
+#     product_result *= int(digit)
+# print("result:", product_result)
+######################################
+
+#Завдання 2
+# Side = int(input("Enter the length: "))
+# Height = int(input("Enter the height: "))
+# area = Side * Height
+# print("Result:", area)
+#
+#git
