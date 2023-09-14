1b5cac25585fec6010e5b313af79931f59e57d43
o.savchuk.arroyo <arroyosavcuk@gmail.com>
2023-09-14 18:36:28 +0200

    Homework 4

---
+++ main.py
@@ -0,0 +1,42 @@
+#Завдання 1 тут не знаю как вывести цифры и как сделать через цикл.
+# message = input("Enter sentence:")
+# print(len(message))
+
+# Завдання 2 - тут вроде работает
+# sentence = input("Enter sentence: ")
+# for letter in sentence:
+#     if letter == 'e':
+#         print(letter, end=" ")
+
+# Завдання 3 - не понимаю как искать и заменить именно целое слово. Буквы как заменить еще поянтно было. Ниже жалкие попытки
+
+# sentence = input("Enter sentence: ")
+#  The quick brown fox jumps over the lazy dog.
+# for word in sentence:
+#     word ==
+#
+#     print()
+
+
+# sentence = input("Enter sentence:")
+# for letter in sentence:
+#     if letter == "a":
+#         print(letter, end=" ")
+
+
+
+# Завдання 4 - тут вышло, но не удивлюсь если и тут я ошиблась
+# text = "The quick brown fox jumps over the lazy dog."
+# print(text[2])
+# print(text[41])
+# print(text[0:6])
+# print(text[0:42])
+# print(text[0::2])
+# print(text[1::2])
+# print(text[::-1])
+# print(text[::-2])
+# print(len(text))
+
+
+
+
