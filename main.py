4ed950005435c04459b60f3470fdff4ce547b65b
o.savchuk.arroyo <arroyosavcuk@gmail.com>
2023-09-14 13:23:44 +0200

    Correction 3

--- main.py
+++ main.py
@@ -1,23 +1,27 @@
 # Завдання 1
-
-# user_select = int(input("Enter menu number: "))
-# match user_select:
-#     case 1:
-#         print("Monday")
-#     case 2:
-#         print("Tuesday")
-#     case 3:
-#         print("Wensday")
-#     case 4:
-#         print("Thursday")
-#     case 5:
-#         print("Friday")
-#     case 6:
-#         print("Saturday")
-#     case 7:
-#         print("Sunday")
-#     case _:
-#         print("Incorrect!")
+# try:
+#     week_day_number = int(input("Enter week day number: "))
+#     match week_day_number:
+#         case 1:
+#             print("Monday")
+#         case 2:
+#             print("Tuesday")
+#         case 3:
+#             print("Wensday")
+#         case 4:
+#             print("Thursday")
+#         case 5:
+#             print("Friday")
+#         case 6:
+#             print("Saturday")
+#         case 7:
+#             print("Sunday")
+#         case _:
+#             print("Incorrect week day number")
+# except ValueError:
+#     print("Enter only numbers")
+# except Exception as error:
+#     print(error)
 ##################################
 # Завдання 2
 # try:
@@ -25,59 +29,52 @@
 #     num2 = int(input("Enter second number: "))
 #     result = (num1 >num2)
 #     print(f"Result: {result}")
-# except ValueEror as error:
-#     print("Enter only integer numbers!")
+# except ValueError as error:
+#     print("Enter only numbers!")
 #     print(f"ValueError: {error}")
 # else:
 #     min_num = min(num1, num2)
 #     max_num = max(num1, num2)
-
-    # print(f" {min_num}, {max_num}")
+#
+#
+# finally:  print(f" {min_num}, {max_num}")
+#Завдання 2 V2
+# try:
+#     first_number = int(input("Enter first number: "))
+#     second_number = int(input("Enter second number: "))
+#     if first_number > second_number:
+#         print(second_number, "and", first_number)
+#     elif second_number > first_number:
+#         print(first_number,"and", second_number)
+#     else:
+#         print("Equals")
+# except ValueError:
+#     print("Enter only number please")
+# except Exception as error:
+#     print(error)
 #############################
 
 # Завдання 3
+
 # try:
 #     num1 = int(input("Enter first number: "))
 #     num2 = int(input("Enter second number: "))
-#     result = (num1 / num2)
-#     print(f"Result: {result}")
+#     math_action = input("Enter math action: + - * /")
+#
+#     match math_action:
+#         case "+":
+#             print(num1, math_action, num2, "=", num1 + num2)
+#         case "-":
+#             print(num1, math_action, num2, "=", num1 - num2)
+#         case "*":
+#             print(num1, math_action, num2, "=", num1 * num2)
+#         case "/":
+#             print(num1, math_action, num2, "=", num1 / num2)
+#         case _:
+#             raise Exception("Incorrect math action")
+# except ValueError:
+#     print("Enter only number please")
 # except ZeroDivisionError as error:
-#     print("impossible!")
-#     print(f"Error: {error}")
-
-# select = input("Choose operation: (+, -, *, /): ")
-# num1 = int(input("Enter first number: "))
-# num2 = int(input("Enter second number: "))
-#
-# if select == "+":
-#     result = (num1 + num2)
-#
-#     print(f"Result: {result}")
-#
-# elif select == "-":
-#     result = (num1 - num2)
-#
-#     print(f"Result: {result}")
-#
-#
-# elif select == "*":
-#     result = (num1 * num2)
-#
-#     print(f"Result: {result}")
-#
-# elif select == "/":
-#     result = (num1 / num2)
-#     # if num2 != 0:
-#     # print(f"Result: {result}")
-#
-#
-# # ZeroDivisionError
-# # print(f"Zero division impossible: {error}")
-
-
-
-
-
-
-
-
+#     print("impossible to divide by zero")
+# except Exception as error:
+#     print(error)
